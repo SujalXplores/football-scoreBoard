4c13a2eb7a64a36e77a95d94c3a8af4d5ec39f0c
SujalShah3234 <shahc9437@gmail.com>
2021-12-21 23:12:14 +0530

    ⚡️ Cutting out unused imports from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { StandingsComponent } from './components/standings/standings.component';
 import { StandingsService } from './services/standings.service';
 import { Interceptor } from "./services/app.interceptor";
@@ -36,22 +28,13 @@ import { Interceptor } from "./services/app.interceptor";
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    MatCheckboxModule,
-    MatCheckboxModule,
     MatButtonModule,
-    MatInputModule,
     MatAutocompleteModule,
-    MatDatepickerModule,
     MatFormFieldModule,
     MatSelectModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatMenuModule,
     MatToolbarModule,
-    MatButtonToggleModule,
     MatIconModule,
-    MatTableModule,
-    MatPaginatorModule,
+    MatTableModule
   ],
   providers: [
     HttpClient,
